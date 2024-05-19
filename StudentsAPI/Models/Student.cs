1fab6ea0b032d50a52c630a1abc62de4200994ad
lukmik3 <125919991+lukmik3@users.noreply.github.com>
2024-05-19 19:35:46 +0300

    initial

---
+++ StudentsAPI/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentsAPI.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public required string LastName { get; set; }
+		public required string FirstMidName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+		public ICollection<Enrollment>? Enrollments { get; set; }
+	}
+}
